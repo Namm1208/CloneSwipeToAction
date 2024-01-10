03748c86f710d78dad2dae40642889e0b52675ba
Namm1208 <125287992+Namm1208@users.noreply.github.com>
2024-01-10 23:35:37 +0700

    giao diẹn

--- app/src/main/java/com/example/cloneswipetoaction/MainActivity.java
+++ app/src/main/java/com/example/cloneswipetoaction/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.cloneswipetoaction;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private YourAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        List<String> dataList = generateSampleData();
+
+
+        adapter = new YourAdapter(dataList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    // Phương thức để tạo dữ liệu mẫu
+    private List<String> generateSampleData() {
+        List<String> data = new ArrayList<>();
+        data.add("Book 1");
+        data.add("Book 2");
+        data.add("Book 3");
+
+        return data;
     }
 }
