30ed73d7e47ae43f6404ae3f8be2d721a702e01d
Namm1208 <125287992+Namm1208@users.noreply.github.com>
2024-01-14 23:49:19 +0700

    fix các lỗi

--- app/src/main/java/com/example/cloneswipetoaction/Adapter/MyAdapter.java
+++ app/src/main/java/com/example/cloneswipetoaction/Adapter/MyAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide; // Add this import
 import com.example.cloneswipetoaction.Model.Item;
 import com.example.cloneswipetoaction.R;
 
@@ -27,15 +28,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(context)
-                .inflate(R.layout.item_layout, parent, false);
+                .inflate(R.layout.items, parent, false);
         return new MyViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-       Gilde.with(context).load(itemList.get(position).getImage().into(holder.cart_item_img));
-       holder.cart_item_name.setText(itemList.get(position).getName());
-       holder.cart_item_price.setText(itemList.get(position).getPrice());
+        Glide.with(context).load(itemList.get(position).getImage()).into(holder.cart_item_img);
+        holder.cart_item_name.setText(itemList.get(position).getName());
+        holder.cart_item_price.setText(itemList.get(position).getPrice());
     }
 
     @Override
--- app/src/main/java/com/example/cloneswipetoaction/MainActivity.java
+++ app/src/main/java/com/example/cloneswipetoaction/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.cloneswipetoaction;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cloneswipetoaction.Adapter.MyAdapter;
+import com.example.cloneswipetoaction.Model.Item;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,32 +16,32 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private YourAdapter adapter;
+    private MyAdapter adapter;
+    private LinearLayoutManager layoutManager;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Init
+        recyclerView = findViewById(R.id.recyclerView); // Sửa đổi id thành recyclerView
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-
-        List<String> dataList = generateSampleData();
-
-
-        adapter = new YourAdapter(dataList);
-        recyclerView.setAdapter(adapter);
+        generateItem();
     }
 
-    // Phương thức để tạo dữ liệu mẫu
-    private List<String> generateSampleData() {
-        List<String> data = new ArrayList<>();
-        data.add("Book 1");
-        data.add("Book 2");
-        data.add("Book 3");
-
-        return data;
+    private void generateItem() {
+        List<Item> itemList = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            itemList.add(new Item("pie 0" + (i + 1),
+                    "100000",
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fphotos%2Ffood&psig=AOvVaw1AwfyRavgs2pfNTHWduS5_&ust=1705336526923000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNCn8uKn3YMDFQAAAAAdAAAAABAD"));
+        }
+        adapter = new MyAdapter(this, itemList);
+        recyclerView.setAdapter(adapter);
     }
 }
