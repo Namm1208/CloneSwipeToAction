8c63132906818e29e4c68aa11b7fc5b740f5a6ed
Namm1208 <125287992+Namm1208@users.noreply.github.com>
2024-01-18 06:57:25 +0700

    update

--- app/src/main/java/com/example/cloneswipetoaction/Adapter/BooksAdapter.java
+++ app/src/main/java/com/example/cloneswipetoaction/Adapter/BooksAdapter.java
@@ -1,47 +1,60 @@
 package com.example.cloneswipetoaction.Adapter;
 
 import android.net.Uri;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.RequestManager;
+import com.example.cloneswipetoaction.Model.Book;
+import com.example.cloneswipetoaction.R;
+import com.example.cloneswipetoaction.SwipeToAction;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 import java.util.List;
 
-import co.dift.ui.SwipeToAction;
 
-
-public class BooksAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.ViewHolder> {
 
     private List<Book> items;
 
-
-    /** References to the views for each data item **/
-    public class BookViewHolder extends SwipeToAction.ViewHolder<Book> {
+    public class ViewHolder extends SwipeToAction.ViewHolder<Book> {
         public TextView titleView;
         public TextView authorView;
-        public SimpleDraweeView imageView;
+        public View imageView;
+        public View revealLeftView;
+        public View revealRightView;
+        public Book data;
 
-        public BookViewHolder(View v) {
-            super(v);
+        public ViewHolder(View v) {
+            super();
 
-            titleView = (TextView) v.findViewById(R.id.title);
-            authorView = (TextView) v.findViewById(R.id.author);
-            imageView = (SimpleDraweeView) v.findViewById(R.id.image);
+            titleView = v.findViewById(R.id.title);
+            authorView = v.findViewById(R.id.author);
+            imageView = v.findViewById(R.id.image);
+            revealLeftView = v.findViewById(R.id.reveal_left);
+            revealRightView = v.findViewById(R.id.reveal_right);
+        }
+
+        @Override
+        public void onItemSelected() {
+            // Xử lý khi item được chọn
+        }
+
+        @Override
+        public void onItemClear() {
+            // Xử lý khi item được clear
+        }
+
+        public void setSwipeListener(SwipeToAction.SwipeListener<Book> bookSwipeListener) {
         }
     }
 
-    /** Constructor **/
-    public BooksAdapter(List<Book> items) {
+    public BooksAdapter(List<Book> items, RequestManager with) {
         this.items = items;
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        return 0;
     }
 
     @Override
@@ -50,20 +63,47 @@ public class BooksAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_view, parent, false);
-
-        return new BookViewHolder(view);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
         Book item = items.get(position);
-        BookViewHolder vh = (BookViewHolder) holder;
-        vh.titleView.setText(item.getTitle());
-        vh.authorView.setText(item.getAuthor());
-        vh.imageView.setImageURI(Uri.parse(item.getImageUrl()));
-        vh.data = item;
+        holder.titleView.setText(item.getTitle());
+        holder.authorView.setText(item.getAuthor());
+        holder.imageView.setImageURI(Uri.parse(item.getImageUrl()));
+
+        // Thiết lập sự kiện swipe
+        holder.setSwipeListener(new SwipeToAction.SwipeListener<Book>() {
+            @Override
+            public boolean swipeLeft(Book itemData) {
+                // Xử lý khi swipe sang trái
+                holder.revealRightView.setVisibility(View.GONE);
+                holder.revealLeftView.setVisibility(View.VISIBLE);
+                return true; // true nếu muốn hiển thị hiệu ứng swipe, ngược lại là false
+            }
+
+            @Override
+            public boolean swipeRight(Book itemData) {
+                // Xử lý khi swipe sang phải
+                holder.revealLeftView.setVisibility(View.GONE);
+                holder.revealRightView.setVisibility(View.VISIBLE);
+                return true; // true nếu muốn hiển thị hiệu ứng swipe, ngược lại là false
+            }
+
+            @Override
+            public void onClick(Book itemData) {
+                // Xử lý khi item được click
+            }
+
+            @Override
+            public void onLongClick(Book itemData) {
+                // Xử lý khi item được long click
+            }
+        });
+
+        holder.data = item;
     }
 }
--- app/src/main/java/com/example/cloneswipetoaction/MainActivity.java
+++ app/src/main/java/com/example/cloneswipetoaction/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.cloneswipetoaction;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.cloneswipetoaction.Adapter.BooksAdapter;
 import com.example.cloneswipetoaction.Model.Book;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -20,7 +18,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
-    co.dift.ui.sample.BooksAdapter adapter;
+    BooksAdapter adapter;
     SwipeToAction swipeToAction;
 
     List<Book> books = new ArrayList<>();
@@ -35,7 +33,57 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
 
-        adapter = new co.dift.ui.sample.BooksAdapter(this.books, Glide.with(this));
+        // Khởi tạo danh sách sách mẫu (điều này cần thay đổi theo nhu cầu của bạn)
+        initializeBooks();
+
+        // Khởi tạo adapter
+        adapter = new BooksAdapter(books, Glide.with(this));
+
+        // Thiết lập SwipeToAction
+        swipeToAction = new SwipeToAction(recyclerView, new SwipeToAction.SwipeListener<Book>() {
+            @Override
+            public boolean swipeLeft(Book itemData) {
+                // Xử lý khi swipe sang trái
+                showSnackbar("Swiped Left: " + itemData.getTitle());
+                return true; // true nếu bạn muốn hiển thị hiệu ứng swipe, ngược lại là false
+            }
+
+            @Override
+            public boolean swipeRight(Book itemData) {
+                // Xử lý khi swipe sang phải
+                showSnackbar("Swiped Right: " + itemData.getTitle());
+                return true; // true nếu bạn muốn hiển thị hiệu ứng swipe, ngược lại là false
+            }
+
+            @Override
+            public void onClick(Book itemData) {
+                // Xử lý khi item được click
+                showSnackbar("Clicked: " + itemData.getTitle());
+            }
+
+            @Override
+            public void onLongClick(Book itemData) {
+                // Xử lý khi item được long click
+                showSnackbar("Long Clicked: " + itemData.getTitle());
+            }
+        });
+
+        // Bổ sung ItemTouchHelper vào SwipeToAction
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(swipeToAction.getCallback());
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
         recyclerView.setAdapter(adapter);
+    }
+
+    private void initializeBooks() {
+        // Thêm các sách mẫu vào danh sách
+        // Điều này cần thay đổi tùy thuộc vào cách bạn quản lý dữ liệu sách
+        for (int i = 1; i <= 20; i++) {
+            books.add(new Book("Book " + i, "Author " + i, "https://example.com/book" + i + ".jpg"));
+        }
+    }
+
+    private void showSnackbar(String message) {
+        Snackbar.make(recyclerView, message, Snackbar.LENGTH_SHORT).show();
     }
 }
