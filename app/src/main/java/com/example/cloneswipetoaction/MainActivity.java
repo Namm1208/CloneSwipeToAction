5e46899484391e3ff73bd18e5f464b80e3246f94
Namm1208 <125287992+Namm1208@users.noreply.github.com>
2024-01-17 23:03:23 +0700

    update

--- app/src/main/java/com/example/cloneswipetoaction/MainActivity.java
+++ app/src/main/java/com/example/cloneswipetoaction/MainActivity.java
@@ -1,47 +1,41 @@
 package com.example.cloneswipetoaction;
 
-import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.cloneswipetoaction.Adapter.MyAdapter;
-import com.example.cloneswipetoaction.Model.Item;
+import com.bumptech.glide.Glide;
+import com.example.cloneswipetoaction.Model.Book;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private MyAdapter adapter;
-    private LinearLayoutManager layoutManager;
-    private Context context;
+    RecyclerView recyclerView;
+    co.dift.ui.sample.BooksAdapter adapter;
+    SwipeToAction swipeToAction;
+
+    List<Book> books = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Init
-        recyclerView = findViewById(R.id.recyclerView); // Sửa đổi id thành recyclerView
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
+        recyclerView = findViewById(R.id.recycler);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
 
-        generateItem();
-    }
-
-    private void generateItem() {
-        List<Item> itemList = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
-            itemList.add(new Item("pie 0" + (i + 1),
-                    "100000",
-                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fphotos%2Ffood&psig=AOvVaw1AwfyRavgs2pfNTHWduS5_&ust=1705336526923000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNCn8uKn3YMDFQAAAAAdAAAAABAD"));
-        }
-        adapter = new MyAdapter(this, itemList);
+        adapter = new co.dift.ui.sample.BooksAdapter(this.books, Glide.with(this));
         recyclerView.setAdapter(adapter);
     }
 }
